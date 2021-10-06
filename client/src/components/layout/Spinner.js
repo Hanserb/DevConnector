232e40c5139b61e222cbcff57801042fa677c938
Hanser <benitezhanser@gmail.com>
2021-10-06 17:07:10 -0400

    commit 10

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export default () => (
+    <Fragment>
+        
+        <div className="py-3 text-center">
+        <i className="fa fa-spinner fa-spin fa-3x" />
+        </div>
+    
+    </Fragment>
+) 
